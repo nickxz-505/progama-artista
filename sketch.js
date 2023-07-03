1a9010c96bf7b7b82bd720d0439b73b9f819e207
nickxz-505 <137806326+nickxz-505@users.noreply.github.com>
2023-07-03 12:04:12 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(600, 600);
+   background("#030303(0;0;0;0.95)");
+}
+
+function draw() { 
+  fill("violet")
+  if(mouseIsPressed)
+  rect(mourgb(0,0,0)ouseY,20,35); }
+  stroke("black")
